29d8a5b370a75158b808ada79ff65d5d74f33930
amritaarora0902 <amritaarora2424@gmail.com>
2021-06-10 17:48:19 +0530

    requestdipatcher

--- src/main/java/com/amrita/AddServlet.java
+++ src/main/java/com/amrita/AddServlet.java
@@ -3,6 +3,8 @@ package com.amrita;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,13 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i+j;
-		PrintWriter out = res.getWriter();
-		out.println(k);
+//		PrintWriter out = res.getWriter();
+//		out.println(k);
+		
+		//To share data within servlet we use the concept of session management
+		req.setAttribute("k", k);
+		
+		//To call the different servlet we have to option 1)RequestDispatcher 2)Redirect
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
+		
+		
+		
 	}
 }
