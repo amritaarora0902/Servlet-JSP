487b3f9b189ff38339f6e1cd110a0f0f79c2e883
amritaarora0902 <amritaarora2424@gmail.com>
2021-06-11 13:25:38 +0530

    servlet-annotations

--- src/main/java/com/amrita/AddServlet.java
+++ src/main/java/com/amrita/AddServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
