a31401e63a53ffec8cab5614772fc678568d5ee2
amritaarora0902 <amritaarora2424@gmail.com>
2021-06-11 12:29:37 +0530

    cookies

--- src/main/java/com/amrita/SqServlet.java
+++ src/main/java/com/amrita/SqServlet.java
@@ -3,17 +3,26 @@ package com.amrita;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 public class SqServlet extends HttpServlet {
 		
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
 		
-		HttpSession session = req.getSession();
-		int k = (int) session.getAttribute("k");
+		int k=0;
+		Cookie cookies[] = req.getCookies();
+		
+		for(Cookie co : cookies) {
+			if(co.getName().equals("k")) {
+				k =  Integer.parseInt(co.getValue());
+			}
+		}
+		
 		k=k*k;
 		PrintWriter out = res.getWriter();
 		out.println("result is: "+k);
